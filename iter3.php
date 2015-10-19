d5ea026d8d6f6d89985144a85ac0f3339072a248
Damien Harwin <damien.harwin@mayden.co.uk>
2015-10-19 11:25:28 -0700

    WIP iter3

--- iter3.php
+++ iter3.php
@@ -1,1 +1,22 @@
 <?php
+class Cell {
+    const ALIVE = 1;
+    const DEAD = 0;
+    public $xpos;
+    public $ypos;
+    public $liveNeighboursCount;
+    public $nextState;
+
+    public function evolve()
+    {
+        $live = $dead = 0;
+        foreach ( [-1,0,1] as $xoff) {
+            foreach( [-1,0,1] as $yoff) {
+                // implement ruleset
+                if($xoff == 0 && $yoff == 0)
+                    continue;
+                
+            }
+        }
+    }
+}
